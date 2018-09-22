d40a8f906fea8a64cc2b58ad65560aa678a98b63
Moonlor <Kenneth0875@gmail.com>
2018-09-22 21:45:09 +0800

    [Fix] trying to save from a disaster....

--- middleware/authority.js
+++ middleware/authority.js
@@ -12,22 +12,19 @@ function checkIfAllow(request) {
 var checkAuthority = function () {
 
     return async (ctx, next) => {
-
-        await next();
-
-        // if(checkIfAllow(ctx.request)) {
-        //     await next();
-        // }
-        // else {
-        //     var id = ctx.cookies.get('sessionId');
-        //     let session = await SessionRepository.findOne({id: id});
-        //     if(session == null) {
-        //         ctx.response.body = ResponseService.createAuthResponse();
-        //     }
-        //     else {
-        //         await next();
-        //     }
-        // }
+        if(checkIfAllow(ctx.request)) {
+            await next();
+        }
+        else {
+            var id = ctx.cookies.get('sessionId');
+            let session = await SessionRepository.findOne({id: id});
+            if(session == null) {
+                ctx.response.body = ResponseService.createAuthResponse();
+            }
+            else {
+                await next();
+            }
+        }
     };
 };
 
--- utils/config.js
+++ utils/config.js
@@ -1,22 +1,23 @@
 var config = {
-  database: 'RegenIn',
-  username: 'test',
-  password: 'test',
-  host: '212.64.17.49',
+  database: 'regenInc',
+  username: 'root',
+  password: 'Aa123456!',
+  host: '101.132.77.182',
   port: 3306,
   dialect: 'mysql',
   ACCESS_KEY: 'opKzqbb_E-y_yKjCufAu8KOiBzlhMXH2G-EXuKVj',
   SECRET_KEY: 'b6pyxBh1o8PCNALdlihIZv54wOxqrbxVohrRLQPH',
-  bucket: 'infotest',
+  bucket: 'inforegen',
   filePath: '../img/',
   // download: 'http://oq7eluo6z.bkt.clouddn.com/'
-  download: 'http://pc0bksa0g.bkt.clouddn.com/',
+  // download: 'http://pc0bksa0g.bkt.clouddn.com/',
+  download: 'http://ovwnlb7r9.bkt.clouddn.com/',
 
   mail_host: 'smtp.sina.com',
   mail_user: 'regeneration_ms@sina.com',
   mail_password: 'regeneration_ms',
 
-  mail_to: ['1061152718@qq.com', 'kenneth0875@gmail.com'],
+  mail_to: ['1061152718@qq.com', 'info@regen.org.cn'],
   homepage_video_id : 21,
 
 
